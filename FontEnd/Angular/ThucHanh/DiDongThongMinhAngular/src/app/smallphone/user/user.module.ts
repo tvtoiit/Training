915fed4d40b41cdf897a7237bf00d874ad3567f2
vantoicntt0@gmail.com <TranVanToi234>
2024-12-07 23:14:48 +0700

    add component detail

--- FontEnd/Angular/ThucHanh/DiDongThongMinhAngular/src/app/smallphone/user/user.module.ts
+++ FontEnd/Angular/ThucHanh/DiDongThongMinhAngular/src/app/smallphone/user/user.module.ts
@@ -6,10 +6,12 @@ import { SharedModule } from '../../share/shared.module';
 import { HomeComponent } from './component/home/home.component';
 import { SizeBarComponent } from './component/home/sizebar/sizebar.component';
 import { ContentProduct } from './component/home/contentproduct/contentproduct.component';
+import { DetailComponent } from './component/detail/detail.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    DetailComponent,
     ContentProduct, 
     SizeBarComponent
   ],
